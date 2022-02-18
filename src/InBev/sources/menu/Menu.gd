5dc381b59da2fb78f2c8cef9a16aed792e427efd
ViniciosLugli <vinicioslugli@gmail.com>
2022-02-17 22:51:37 -0300

    Update anchors for all menu base

--- src/InBev/sources/menu/Menu.gd
+++ src/InBev/sources/menu/Menu.gd
@@ -5,10 +5,10 @@ var gameScene
 signal go_to_game
 
 func _ready():
-	connect("go_to_game", get_node("/root/MainScene"), "_on_change_to_game")
+	connect("go_to_game", get_node("/root/MainScene"), "_change_scene_to")
 	$AnimationPlayerMenu.play("ModulateVariation")
 
 func _on_StartButton_pressed():
-	$StartButton.modulate = Color(0.95, 0.95, 0.95, 1)
-	emit_signal("go_to_game")
+	$CenterContainer/StartButton.modulate = Color(0.95, 0.95, 0.95, 1)
+	emit_signal("go_to_game", "res://scenes/Game.tscn")
 
