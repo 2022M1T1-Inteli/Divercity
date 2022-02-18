a4f88b432bfcd3808271b5cb9b38daa1f322f407
ViniciosLugli <vinicioslugli@gmail.com>
2022-02-18 11:21:10 -0300

    Added comments to codes

--- src/InBev/sources/Main.gd
+++ src/InBev/sources/Main.gd
@@ -1,12 +1,21 @@
 extends Node
 
+# Load the menu node
 var menu = load("res://scenes/Menu.tscn")
 
 func _ready():
+	# Add the menu scene to the scene stack
 	add_child(menu.instance())
 
-func _change_scene_to(scene):
+func _change_scene_to(scene): # Callback to change scene from childs
+	# Set propriety of the fade transition node
 	$FadeAnimator.interpolate_property(get_node("CanvasLayer/Overlay"), "color", Color(0, 0, 0, 0), Color(0, 0, 0, 1), 1.0, Tween.TRANS_LINEAR, Tween.EASE_IN)
+
+	# Start fade animation
 	$FadeAnimator.start()
+
+	# Wait tween finish animation
 	yield($FadeAnimator, "tween_completed")
+
+	# Change tree scene for the new scene
 	get_node("Menu").get_tree().change_scene_to(load(scene))
--- src/InBev/sources/menu/Menu.gd
+++ src/InBev/sources/menu/Menu.gd
@@ -1,14 +1,19 @@
 extends Control
 
-var gameScene
-
+# Create local signal for change to game scene
 signal go_to_game
 
 func _ready():
+	# Connect callback for local signal
 	connect("go_to_game", get_node("/root/MainScene"), "_change_scene_to")
+
+	# Init main moon light animation
 	$AnimationPlayerMenu.play("ModulateVariation")
 
-func _on_StartButton_pressed():
+func _on_StartButton_pressed(): # Callback on click start button
+	# Change button light for response click
 	$CenterContainer/StartButton.modulate = Color(0.95, 0.95, 0.95, 1)
+
+	# Emit signal to change scene
 	emit_signal("go_to_game", "res://scenes/Game.tscn")
 
