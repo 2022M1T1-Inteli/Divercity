24ec951bdbe9bc14cf4e6388429fc82fc27159f9
ViniciosLugli <vinicioslugli@gmail.com>
2022-03-17 00:27:12 -0300

    Added eletropipe functions to basic work

--- src/InBev/sources/minigames/eletropipe/Pipe.gd
+++ src/InBev/sources/minigames/eletropipe/Pipe.gd
@@ -2,7 +2,27 @@ extends Area2D
 
 var currentPosition = 0
 
-export(Array) var outputs = []
+
+const MAX_TILE_SIZE = Vector2(4, 6)
+
+export(Array) var holes = []
+export var tilePosition = Vector2(0, 0)
+export var canRotate = true
+
+export var type = "NORMAL"
+"""
+	NORMAL - basic pipe
+	ENTER - start pipe
+	EXIT - exit pipe
+"""
+
+static func tile_to_position(tile):
+	tile.x = MAX_TILE_SIZE.x if tile.x > MAX_TILE_SIZE.x else tile.x # Clamp position.x to defaultSize.x
+	tile.y = MAX_TILE_SIZE.y if tile.y > MAX_TILE_SIZE.y else tile.y # Clamp position.y to defaultSize.y
+
+	tile.x = 0 if tile.x < 0 else tile.x # Clamp position.x to 0
+	tile.y = 0 if tile.y < 0 else tile.y # Clamp position.y to 0
+	return Vector2(72 + (144 * tile.x), 72 + (144 * tile.y)) # Return the position of the tile
 
 static func get_side_vector(side):
 	"""
@@ -23,21 +43,40 @@ static func get_side_vector(side):
 	else:
 		return Vector2.ZERO # not implemented
 
+static func rotate_array(array, direction = 1):
+	"""
+		Steps the array in the given direction.
+		1 = right
+		-1 = left
+	"""
+	direction = direction if direction == 1 or direction == -1 else 1 # Clamp direction to 1 or -1
+
+	direction = -direction # Invert direction for set positive value a right direction
+
+	var movedArray = [] # Create a new array
+
+	for i in range(0, len(array)): # For each element in the array
+		movedArray.append(array[(i + direction) % len(array)]) # Add the element of move to the new array
+
+	return movedArray # Return the new array
+
 func _ready():
+	set_tile(tilePosition)
+
 	var err = self.connect("input_event", self, "_on_Pipe_input_event") # connect to the input event
 
 	if err: # if there was an error
 		print("Error connecting signal on input_event: ", err) # print the error
 
-func get_output_sides():
+func get_holes_sides():
 	"""
-		Returns the sides of the outputs in array of vectors.
+		Returns the sides of the holes in array of vectors.
 	"""
 
 	var connects = [] # create a new array to hold the connections vectors
 
-	for outputIdx in range(outputs.size()): # for each output
-		if outputs[outputIdx]: # if there is a connection
+	for outputIdx in range(holes.size()): # for each output
+		if holes[outputIdx]: # if there is a connection
 			connects.append(get_side_vector(outputIdx)) # add the connection vector to the array
 
 	return connects # return the array of vectors of the connections
@@ -50,9 +89,16 @@ func rotate_pipe(direction = 1):
 			-1 = left
 			0 = no rotation
 	"""
+	if not canRotate: # if the pipe can't rotate
+		return
+
+	direction = direction if direction == 1 or direction == -1 else 1 # Clamp direction to 1 or -1
 	currentPosition = currentPosition + (90 * direction) # Rotate 90 degrees from current position in direction
+
 	$Tween.interpolate_property(self, "rotation_degrees", self.rotation_degrees, currentPosition, 0.3, Tween.TRANS_LINEAR, Tween.EASE_IN) # Rotation animation
 	$Tween.start() # Start rotate animation
+
+	holes = rotate_array(holes, direction) # Rotate the holes array
 
 func _on_Pipe_input_event(_viewport, event, _shape_idx): # on input event
 	"""
@@ -60,3 +106,11 @@ func _on_Pipe_input_event(_viewport, event, _shape_idx): # on input event
 	"""
 	if event is InputEventScreenTouch and event.is_pressed(): # If has touch event and is pressed
 		rotate_pipe() # Rotate pipe
+
+func set_tile(tile):
+	"""
+		Sets the position of the pipe.
+	"""
+	self.position = tile_to_position(tile) # Set position
+	tilePosition = tile # Set the tile position
+	name = "PIPE_%dx%d" % [tilePosition.x, tilePosition.y] # Set the name of the pipe
--- src/InBev/sources/minigames/eletropipe/Table.gd
+++ src/InBev/sources/minigames/eletropipe/Table.gd
@@ -1,73 +1,142 @@
 extends Node2D
 
-export(Vector2) var size = Vector2.ZERO
+const MAX_TILE_SIZE = Vector2(4, 6)
 
-class BasePipe:
-	var rotation = 0
-	var type = 0
-	var position = Vector2.ZERO
+const PIPES_TYPE = {
+	ENTER = "ENTER",
+	PATH = "NORMAL",
+	EXIT = "EXIT"
+}
 
-	var defaultSize = Vector2.ZERO
+const PIPES_NAME = {
+	DEFAULT = "PIPE_"
+}
 
-	func _init(size):
-		defaultSize = size # Set default size
 
-	func rotate(direction):
-		"""
-			Rotates the pipe to direction.
-			Directions:
-				1 = right
-				-1 = left
-				0 = no rotation
-		"""
-		rotation = direction + (90 * direction) # Add 90 degrees to direction to make it relative to the pipe
+onready var pipeCurvedScene = preload("res://scenes/minigames/eletropipe/bases/PipeCurved.tscn")
+onready var pipeStraightScene = preload("res://scenes/minigames/eletropipe/bases/PipeStraight.tscn")
 
-	func set_position(position):
-		"""
-			Sets the position of the pipe.
-		"""
-		position.x = defaultSize.x if position.x > defaultSize.x else position.x # Clamp position.x to defaultSize.x
-		position.y = defaultSize.y if position.y > defaultSize.y else position.y # Clamp position.y to defaultSize.y
-		self.position = position # Set position
-
-func _ready():
-	pass
-
-static func get_pipe_type_name(type):
+func is_pipes_path_connected():
 	"""
-		Return the name of the pipe type.
-		0 = straight
-		1 = curved
+		Return True if the pipes are connected.
 	"""
+	var currentPipeTrack = get_pipe_enter() # Get the current pipe track
+	var endPipe = get_pipe_exit() # get the exit pipe
 
-	if type == 0:
-		return "Straight" # Straight pipe
-	elif type == 1:
-		return "Curved" # Curved pipe
-	elif type == 2:
-		return "" # Empty pipe
-	else:
-		return "Unknown" # not implemented
+	var trackList = [] # list of track
 
-func is_pipes_connected():
-	pass
+	var nextDirection = Vector2.ZERO # The direction of the next pipe
+	var lastDirection = Vector2.ZERO # The direction of the last pipe
+
+	for _pipe in self.get_children(): # Limit the search to the pipes with the numbers of pipes
+		trackList.append(currentPipeTrack) # Add the current pipe track to the list
+
+		for outerDirection in currentPipeTrack.get_holes_sides(): # Get the outer direction of the current pipe
+			if outerDirection * -1 == lastDirection: # If the direction is the same as the last direction
+				continue
+
+			var dumpPipe = get_next_pipe_from_direction(currentPipeTrack.tilePosition, outerDirection) # Get the next pipe from the direction
+
+			if dumpPipe == null:# Check if pipe exist
+				continue
+
+			for innerDirection in dumpPipe.get_holes_sides(): # Get the inner direction of the next pipe
+				if innerDirection * -1 == outerDirection: # If the direction is the same as the outer direction
+					nextDirection = outerDirection # Set the next direction
+					break
+
+			if nextDirection != Vector2.ZERO: # Check if the next direction is not zero and break outer loop
+				break
+
+		if nextDirection == Vector2.ZERO: # Check if the next direction is zero
+			print("[Eletropipe] NOT CONNECT: Next direction not found.")
+			return false
+
+		currentPipeTrack = get_next_pipe_from_direction(currentPipeTrack.tilePosition, nextDirection) # Get the next pipe
+
+		if currentPipeTrack == null:
+			print("[Eletropipe] NOT CONNECT: The pipes are not connected to continue.")
+			return false
+
+		elif currentPipeTrack == endPipe:
+			print("[Eletropipe] CONNECTED: The pipes are connected!.")
+			return true
+
+		lastDirection = nextDirection # Set the last direction
+		nextDirection = Vector2.ZERO # Reset the next direction
+
+func create_pipe(style, tilePosition):
+	"""
+		Create a pipe with the given position and type.
+		Style: The style of the pipe: 'curved' or 'straight'.
+		TilePosition: The position of the pipe.
+	"""
+	var pipe = null # The pipe to be created
+
+	if style == "curved": # If the style is curved
+		pipe = pipeCurvedScene.instance() # Create the pipe
+	elif style == "straight": # If the style is straight
+		pipe = pipeStraightScene.instance() # Create the pipe
+
+	pipe.tilePosition = tilePosition # Set the position of the pipe
+	return pipe # Return the pipe
+
+func get_random_seed():
+	"""
+		Return a random seed.
+	"""
+	var rng = RandomNumberGenerator.new() # Create a new random number generator
+	rng.randomize() # Randomize the number generator
+	return rng.randi() # Return a random integer
+
+#func generate_random_pipes_path():
+#	"""
+#		Generate the pipes path with correct connetions.
+#	"""
+#	# Create pipes path with the correct connections in enter and exit pipes
+#	var enterPipe = get_pipe_enter() # Get the current pipe track
+#	var endPipe = get_pipe_exit() # get the exit pipe
+
+#	var trackList = [] # List of track
+
+#	for _i in range(MAX_TILE_SIZE.x * MAX_TILE_SIZE.y):
+#		var pipe
 
 func init_energy_system():
 	pass
 
-func get_pipe_from_position(position):
+func get_pipe_enter():
 	"""
-		Returns the pipe at the given position.
+		Return the pipe at the entrance.
+	"""
+	for pipe in self.get_children(): # Search for the pipe at the entrance
+		if pipe.type == PIPES_TYPE.ENTER: # If the pipe is at the entrance
+			return pipe # Return the pipe at the entrance
+	return null # Return null if there is no pipe at the entrance
+
+
+func get_pipe_exit():
+	"""
+		Return the pipe at the exit.
+	"""
+	for pipe in self.get_children(): # Search for the pipe at the exit
+		if pipe.type == PIPES_TYPE.EXIT: # If the pipe is at the exit
+			return pipe # Return the pipe at the exit
+	return null # Return null if there is no pipe at the exit
+
+func get_pipe_from_tile(tile):
+	"""
+		Returns the pipe at the given tile position.
 		If there is no pipe at the given position, returns null.
 	"""
-	return get_node_or_null(("Pipe_%dx%d" % [position.x, position.y])) # Return the pipe at the given position.
+	return get_node_or_null(PIPES_NAME.DEFAULT + ("%dx%d" % [tile.x, tile.y])) # Return the pipe at the given position.
 
-func get_next_pipe_from_direction(position, direction):
+func get_next_pipe_from_direction(tile, direction):
 	"""
 		Returns the next pipe in the given direction.
 		If there is no pipe in the given direction, returns null.
 	"""
-	return get_pipe_from_position(position + direction) # Return the pipe in the given direction from position.
+	return get_pipe_from_tile(tile + direction) # Return the pipe in the given direction from position.
 
 func get_all_pipes():
 	"""
@@ -75,8 +144,12 @@ func get_all_pipes():
 	"""
 	var pipes = [] # Create a list to store all pipes.
 
-	for xIdx in range(size.x): # For each x index.
-		for yIdx in range(size.y): # For each y index.
-			pipes.append(get_pipe_from_position(Vector2(xIdx, yIdx))) # Add the pipe at the given position to the list.
+	for xIdx in range(MAX_TILE_SIZE.x): # For each x index.
+		for yIdx in range(MAX_TILE_SIZE.y): # For each y index.
+			pipes.append(get_pipe_from_tile(Vector2(xIdx, yIdx))) # Add the pipe at the given position to the list.
 
 	return pipes # Return all pipes in the game.
+
+
+func _on_Button_pressed():
+	print(is_pipes_path_connected())
