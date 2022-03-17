df4bd2165c461698864381cb5eb2fe7dcd9031cc
ViniciosLugli <vinicioslugli@gmail.com>
2022-03-17 09:18:11 -0300

    Added base of quiz

---
+++ src/InBev/sources/game/quiz/Quiz.gd
@@ -0,0 +1,10 @@
+extends Control
+
+func _on_Answer3Button_pressed():
+	print("Answer3Button pressed")
+
+func _on_Answer2Button_pressed():
+	print("Answer2Button pressed")
+
+func _on_Answer1Button_pressed():
+	print("Answer1Button pressed")
