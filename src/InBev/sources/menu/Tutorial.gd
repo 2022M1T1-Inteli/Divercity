95df3c00130ceac82608ffecb4d52ff22e9534fe
ViniciosLugli <vinicioslugli@gmail.com>
2022-04-14 01:58:15 -0300

    1.5.0

--- src/InBev/sources/menu/Tutorial.gd
+++ src/InBev/sources/menu/Tutorial.gd
@@ -5,8 +5,6 @@ signal change_scene # Create local signal for change to game scene
 export(String) var callbackScenePath = "res://scenes/menu/Menu.tscn"
 export(Dictionary) var callbackSceneParams = {}
 
-#	emit_signal("change_scene", callbackScenePath, true, callbackSceneParams) # Emit the change scene signal to the game scene
-
 var current = 0
 
 func _ready():
@@ -21,7 +19,7 @@ func _construct(mainNode):
 func next_tutorial():
 	current += 1
 	if current >= 4:
-		emit_signal("change_scene", callbackScenePath, true, callbackSceneParams) # Emit the change scene signal to the game scene
+		emit_signal("change_scene", callbackScenePath, false, callbackSceneParams) # Emit the change scene signal to the game scene
 	for i in range (1, 4):
 		get_node("Tutorial" + str(i)).visible = true if current == i else false
 
